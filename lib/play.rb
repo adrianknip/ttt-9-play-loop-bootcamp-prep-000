26094fb35d90a18b9736440004f9657f9e960189
Adrian Nip <nip.adrian@gmail.com>
2018-06-29 20:23:18 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,7 +38,7 @@ end
 # Define your play method below
 
 def play(board)
-    while (board[8] && board[1] && board[2] && board[3] && board[4] && board[5] && board[6] && board[7] && board[0]) != "X"
+    until (board[8] && board[1] && board[2] && board[3] && board[4] && board[5] && board[6] && board[7] && board[0]) != " "
     turn(board)
   end
 end
